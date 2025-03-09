68e71bebf1fbb2fe0264be8e0f4cdec2c7d62b6f
Mingye Wang <arthur200126@gmail.com>
2025-03-09 11:46:23 +0800

    certbot自动重启nginx

--- install/cert.sh
+++ install/cert.sh
@@ -8,9 +8,18 @@ if [[ ! $DOMAIN ]]; then
 fi
 
 yum install -y certbot
+systemctl enable certbot-renew.timer
 systemctl stop nginx
-certbot standalone -d "$DOMAIN" -d "imap.$DOMAIN" -d "smtp.$DOMAIN"
+certbot standalone -d "$DOMAIN" -d "imap.$DOMAIN" -d "smtp.$DOMAIN" -d "www.$DOMAIN"
 rm /etc/ssl/certs/{dovecot,postfix}.pem || true
 ln -s /etc/ssl/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/ssl/certs/dovecot.pem
-ln -s /etc/ssl/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/ssl/certs/postfix.pem 
+ln -s /etc/ssl/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/ssl/certs/postfix.pem
+cat << EOF > /etc/letsencrypt/renewal-hooks/pre/00nginx
+sudo systemctl stop nginx
+EOF
+cat << EOF > /etc/letsencrypt/renewal-hooks/post/00nginx
+chown -R www:root /etc/letsencrypt/
+sudo systemctl start nginx
+EOF
+chmod +x /etc/letsencrypt/renewal-hooks/*/*
 systemctl start nginx
